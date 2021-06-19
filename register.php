10ea60311b935fa7ea7bb8cea6e74f032958f5e9
Arjun3789 <patil.letzpack@gmail.com>
2021-06-19 11:37:35 +0530

    .

--- register.php
+++ register.php
@@ -1,16 +1,20 @@
 <?php
+    //database connection
     $conn=mysqli_connect('localhost','root');
     mysqli_select_db($conn,'mess_management');
-    if ($conn) {
+    if ($conn) 
+    {
     	// echo "Connected";
     	# code...
     }
-    else{
+    else
+    {
     	echo "Not Connected";
+        # code...
     }
 
-    if(isset($_POST['submit'])){
-
+    if(isset($_POST['submit'])){// accessing through submit btn
+        //assigning values
         $Fname=$_POST['FName'];
         $Lname=$_POST['LName'];
         $Pass=$_POST['Pass'];
@@ -18,17 +22,51 @@
         $email=$_POST['Email'];
         $phone=$_POST['Phone'];
 
-        $q = "INSERT INTO `register` VALUES(`Id`,'$Fname','$Lname','$Pass','$Confirm','$email','$phone')";
-        $query = mysqli_query($conn,$q);
-        if(mysqli_query($conn, $query))
-        {
+        // email verification
+        $emailquery = "select * from register where email='$email'";
+        $query = mysqli_query($conn,$emailquery);
+        $emailcount =  mysqli_num_rows($query);
 
+
+        if ($emailcount>0) { //condition of email verification 
+            ?>
+            <script>
+                //pop up message code
+                alert("email already exist");
+                window.location.replace('register.html');
+            </script>
+            <?php
+            # code...
         }
-        ?>
-        <script>
-            alert("Record Inserted Successfully");
-            window.location.replace('login.html');
-        </script>
-        <?php
+        else{
+            if($Pass === $Confirm)// password condition
+            {
+                //insert query... adding details to database
+                $q = "INSERT INTO `register` VALUES(`Id`,'$Fname','$Lname','$Pass','$Confirm','$email','$phone')";
+                $query = mysqli_query($conn,$q);
+                if(mysqli_query($conn, $query))
+                {
+        
+                }
+                ?>
+                <script>
+                    alert("Registered  Successfully");
+                    window.location.replace('login.html');
+                </script>
+                <?php
+                #code....
+           }
+            else
+            { // wrong password code
+                ?>
+                <script>
+                    alert("password does not matched ");
+                    window.location.replace('register.html');
+                </script>
+                <?php
+                #code...
+            }
+
+        }       
     }
 ?>
