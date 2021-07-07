7ca9273fd241d01963794d2138f91546d7650959
Arjun3789 <patil.letzpack@gmail.com>
2021-07-07 19:57:34 +0530

    user panel

---
+++ demo.php
@@ -0,0 +1,55 @@
+<?php
+
+  //database connection
+  $conn=mysqli_connect('localhost','root');
+  mysqli_select_db($conn,'mess_management');
+  if ($conn) 
+  {
+      //echo "Connected";
+      # code...
+  }
+  else
+  {
+      echo "Not Connected";
+      # code...
+  }
+
+  if(isset($_POST['submit']))// accessing through submit btn
+  {
+      //assigning values
+      $email=$_POST['Email'];
+      $Pass=$_POST['Pass'];
+      // query for fetching email and password
+      $sql = "select * from register where email='$email' and pass='$Pass' ";
+      $result = mysqli_query($conn,$sql);
+
+
+      if($result->num_rows > 0)// checking for email exist or not
+      {
+          $row = mysqli_fetch_assoc($result);
+          ?>
+          <script>
+              //pop up message code for login
+              alert("LogIn sucessful");
+              window.location.replace('main.html');
+          </script>
+          <?php
+          #code...
+      }
+      else
+      {
+          ?>
+          <script>
+              //pop up message code
+              alert("Email or Password are not correct");
+              window.location.replace('login.html');
+          </script>
+          <?php
+          #code...
+      }     
+  }
+
+
+
+
+?>
